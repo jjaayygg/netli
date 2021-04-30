125255e679a3677451c94c708a0a82b41f480e7f
Katacoda Scenario <scenario@katacoda.com>
2021-04-30 18:08:56 +0000

    first

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+app.listen(900,()=>{console.log('listening on port 900')})
+req.get('/here',(res,res)=>{
+  res.send('here is the fucking text')
+})
+
+
+
