f318eaef2788bb48f0dc8d168faab0e25c67f00e
jjaayygg <83363760+jjaayygg@users.noreply.github.com>
2021-05-22 00:36:38 +0530

    app.get

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 900
 
 app.listen(port,()=>{console.log('listening on port'+port)})
-req.get('/',(req,res)=>{
+app.get('/',(req,res)=>{
   res.send('here is the fucking text')
 })
 
