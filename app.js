4fca1bcdb27320f658c1fdbb7d4383f701a6ed36
jjaayygg <83363760+jjaayygg@users.noreply.github.com>
2021-05-22 00:49:40 +0530

    1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT 
+const port = process.env.PORT ||1
 
 app.listen(port,()=>{console.log('listening on port'+port)})
 app.get('/',(req,res)=>{
